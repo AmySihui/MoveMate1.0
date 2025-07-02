e827283e13ea7908174d583a8ccebd3cbbc0dbce
Amy <amysihui1996@gmail.com>
2025-07-02 20:19:35 +0100

    init

---
+++ Server/src/main/java/com/movemate/server/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.movemate.server.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "MoveMate backend is running!";
+    }
+}
